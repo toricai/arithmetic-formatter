672217157bf7efab648fce4ce5df3549b4dda7f5
Vicky Cai <shengmeicai@gmail.com>
2022-05-22 13:54:28 -0400

    Return instead of print

--- arithmetic_arranger/arithmetic_arranger.py
+++ arithmetic_arranger/arithmetic_arranger.py
@@ -3,7 +3,7 @@
 
 def arithmetic_arranger(arr, answers = False):
     """Plan:
-    print one line at a time
+    solution += one line at a time
     max number length = n
     first line: right justified numbers (width n+2), 4 characters of padding in between
     second line: one-character operator, right-justified numbers (width n+1), 4 char padding
@@ -46,39 +46,38 @@ def arithmetic_arranger(arr, answers = False):
         if (len(bottom[n]) > maxlength[n]):
             maxlength[n] = len(bottom[n])
 
-    # max number length = n
-    # print top line
+    # create top line
+    # (max number length = n)
     # right justified numbers (width n+2), 4 characters of padding in between
     for n in range(0, len(top)):
         if n != 0:
-            print("    ", end='')
-        print(top[n].rjust(maxlength[n] + 2), end='')
+            solution += "    "
+        solution += top[n].rjust(maxlength[n] + 2)
 
-    print()
+    solution += "\n"
 
-    # print second line
+    # add second line
     # one-character operator, right-justified numbers (width n+1), 4 char padding
     for n in range(0, len(operator)):
         if n != 0:
-            print("    ", end='')
-        print(operator[n], end='')
-        print(bottom[n].rjust(maxlength[n] + 1), end='')
+            solution += "    "
+        solution += operator[n]
+        solution += bottom[n].rjust(maxlength[n] + 1)
 
-    print()
+    solution += "\n"
 
-    # print dashes
+    # add dashes
     # dashes (width n+2), 4 char padding
     for n in range(0, len(top)):
         if n != 0:
-            print("    ", end='')
-        print("-" * (maxlength[n] + 2), end='')
+            solution += "    "
+        solution += "-" * (maxlength[n] + 2)
 
-    print()
-
-    # print answers
+    # add answers
     # compute answers with an if statement
     # right justified answers (width n+2), 4 characters of padding in between
     if answers:
+        solution += "\n"
         for n in range(0, len(top)):
             if operator[n] == '+':
                 answer = int(top[n]) + int(bottom[n])
@@ -86,24 +85,18 @@ def arithmetic_arranger(arr, answers = False):
                 answer = int(top[n]) - int(bottom[n])
 
             if n != 0:
-                print("    ", end='')
-            print(str(answer).rjust(maxlength[n] + 2), end='')
+                solution += "    "
+            solution += str(answer).rjust(maxlength[n] + 2)
 
-    print("""
+    return solution
 
-  32         1      9999      523
-+  8    - 3801    + 9999    -  49
-----    ------    ------    -----
-  40     -3800     19998      474
+# functional test
+x = ["32 - 698", "3801 - 2", "45 + 43", "123 + 49"]
+ret = arithmetic_arranger(x, True)
 
-""")
+print(ret)
 
-
-
-arithmetic_arranger( ["32 - 698", "3801 - 2", "45 + 43", "123 + 49"], True)
-
-# x = ["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]
-
-#    32      3801      45      123
-# + 698    -    2    + 43    +  49
-# -----    ------    ----    -----
+assert ret == """   32      3801      45      123
+- 698    -    2    + 43    +  49
+-----    ------    ----    -----
+ -666      3799      88      172""", "Return value didn't match what was expected"
